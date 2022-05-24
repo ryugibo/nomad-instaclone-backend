f5ba842d7c5204d9c7e81c532d188664d0195371
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-05-24 20:34:22 +0900

    :memo: #3.6 | Prisma Client

--- server.js
+++ server.js
@@ -1,33 +1,36 @@
+import { PrismaClient } from "@prisma/client";
 import { ApolloServer, gql } from "apollo-server";
+
+const client = new PrismaClient();
 
 const typeDefs = gql`
   type Movie {
-    id: Int
-    title: String
-    year: Int
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
   }
   type Query {
     movies: [Movie]
     movie: Movie
   }
   type Mutation {
-    createMovie(title: String!): Boolean
+    createMovie(title: String!, year: Int!, genre: String): Movie
     deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    movies: () => [],
+    movies: () => client.movie.findMany(),
     movie: () => ({ title: "hello", year: 2021 }),
   },
   Mutation: {
-    createMovie: (_, { title }) => {
-      console.log(title);
-      return true;
-    },
+    createMovie: (_, { title, year, genre }) =>
+      client.movie.create({ data: { title, year, genre } }),
     deleteMovie: (_, { title }) => {
-      console.log(title);
       return true;
     },
   },

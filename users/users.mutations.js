258ebc4ae668c12a93b47cb864d03456b7fff14b
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-05-25 17:50:04 +0900

    :memo: #4.3 | seeProfile
    - try..catch

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -7,20 +7,26 @@ export default {
       _,
       { firstName, lastName, userName, email, password }
     ) => {
-      const existingUser = await client.user.findFirst({
-        where: { OR: [{ userName }, { email }] },
-      });
-      const uglyPassword = await bcrypt.hash(password, 10);
-
-      return client.user.create({
-        data: {
-          firstName,
-          lastName,
-          userName,
-          email,
-          password: uglyPassword,
-        },
-      });
+      try {
+        const existingUser = await client.user.findFirst({
+          where: { OR: [{ userName }, { email }] },
+        });
+        const uglyPassword = await bcrypt.hash(password, 10);
+        if (existingUser) {
+          throw new Error("This username/email is already taken.");
+        }
+        return client.user.create({
+          data: {
+            firstName,
+            lastName,
+            userName,
+            email,
+            password: uglyPassword,
+          },
+        });
+      } catch (e) {
+        return e;
+      }
     },
   },
 };

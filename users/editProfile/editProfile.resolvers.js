67117094af1133f77d68efe773632a1d04776b78
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-05-25 21:16:24 +0900

    :memo: #4.7 | update Profile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,36 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
 export default {
   Mutation: {
-    editProfile: () => console.log("hi"),
+    editProfile: async (
+      _,
+      { firstName, lastName, userName, email, password: newPassword }
+    ) => {
+      let uglyPassword = null;
+      if (newPassword) {
+        uglyPassword = await bcrypt.hash(newPassword, 10);
+      }
+      const ok = await client.user.update({
+        where: { id: 1 },
+        data: {
+          firstName,
+          lastName,
+          userName,
+          email,
+          ...(uglyPassword && { password: uglyPassword }),
+        },
+      });
+      if (ok) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: "Could not update profile.",
+        };
+      }
+    },
   },
 };

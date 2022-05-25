c18eaca7d8869fb917bba7a65eb87cdc5c60cc55
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-05-25 21:24:39 +0900

    :memo: #4.8 | authentication part One

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,18 +1,20 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import client from "../../client";
 
 export default {
   Mutation: {
     editProfile: async (
       _,
-      { firstName, lastName, userName, email, password: newPassword }
+      { firstName, lastName, userName, email, password: newPassword, token }
     ) => {
+      const { id } = await jwt.verify(token, process.env.SECRET_KEY);
       let uglyPassword = null;
       if (newPassword) {
         uglyPassword = await bcrypt.hash(newPassword, 10);
       }
       const ok = await client.user.update({
-        where: { id: 1 },
+        where: { id: id },
         data: {
           firstName,
           lastName,

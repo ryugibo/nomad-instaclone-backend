dd7edbc8c2e82a9e7444d68046f806db86479e77
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-05-26 20:56:43 +0900

    :memo: #4.17 | File Upload part Four
    - File upload

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,3 +1,4 @@
+import { createWriteStream } from "fs";
 import bcrypt from "bcrypt";
 import client from "../../client";
 import { protectedResolver } from "../users.utils";
@@ -8,8 +9,10 @@ const resolverFn = async (
   { loggedInUser }
 ) => {
   const { filename, createReadStream } = await avatar;
-  const stream = createReadStream();
-  console.log(stream);
+  const readStream = createReadStream();
+  const writeStream = createWriteStream(process.cwd() + "/uploads/" + filename);
+  readStream.pipe(writeStream);
+
   let uglyPassword = null;
   if (newPassword) {
     uglyPassword = await bcrypt.hash(newPassword, 10);

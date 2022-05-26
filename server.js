6614edcd70f05bd773b626effb0545bb0383ca7f
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-05-26 21:29:02 +0900

    :memo: #4.19 | Changing Avatar

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import { getUser } from "./users/users.utils";
 
 const PORT = process.env.PORT;
 
-const server = new ApolloServer({
+const apollo = new ApolloServer({
   typeDefs,
   resolvers,
   context: async ({ req }) => {
@@ -18,7 +18,9 @@ const server = new ApolloServer({
 
 const app = express();
 app.use(logger("tiny"));
-server.applyMiddleware({ app });
+app.use("/static", express.static("uploads"));
+
+apollo.applyMiddleware({ app });
 
 app.listen({ port: PORT }, () => {
   console.log(`Server is running on http://localhost:${PORT}/graphql`);

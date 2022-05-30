60718ecac912beccae5a4e1d8b394c13d1826a35
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-05-30 23:18:31 +0900

    :memo: #4.24 | Followers Pagination part Two

--- users/seeFollowers/seeFollowers.resolvers.js
+++ users/seeFollowers/seeFollowers.resolvers.js
@@ -3,11 +3,26 @@ import client from "../../client";
 export default {
   Query: {
     seeFollowers: async (_, { userName, page }) => {
+      const ok = await client.user.findUnique({
+        where: { userName },
+        select: { id: true },
+      });
+      if (!ok) {
+        return { ok: false, error: "That user does not exist." };
+      }
+      console.log(ok);
       const followers = await client.user
         .findUnique({ where: { userName } })
         .followers({ take: 5, skip: 5 * (page - 1) });
 
-      return { ok: true, followers };
+      const totalFollowers = await client.user.count({
+        where: { following: { some: { userName } } },
+      });
+      return {
+        ok: true,
+        totalPages: Math.ceil(totalFollowers / 5),
+        followers,
+      };
     },
   },
 };

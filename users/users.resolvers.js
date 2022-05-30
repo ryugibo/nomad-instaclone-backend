d7524081c8b103fd88313dd4500800a2fe84d90e
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-05-30 23:49:56 +0900

    :memo: #4.27 | Computed Fields part Two
    - totalFollowing, totalFollowers

--- users/users.resolvers.js
+++ users/users.resolvers.js
@@ -1,6 +1,12 @@
+import client from "../client";
+
 export default {
   User: {
-    totalFollowing: (root) => root.following.length,
-    totalFollowers: (root) => root.followers.length,
+    totalFollowing: ({ id }) => {
+      return client.user.count({ where: { followers: { some: { id } } } });
+    },
+    totalFollowers: ({ id }) => {
+      return client.user.count({ where: { following: { some: { id } } } });
+    },
   },
 };

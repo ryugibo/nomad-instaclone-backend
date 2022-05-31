c2a8b9a41ae688717650c232c55a640e3934bf04
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-05-31 20:17:33 +0900

    :memo: #6.4 | Upload Photo part Three

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -5,27 +5,30 @@ export default {
   Mutation: {
     uploadPhoto: protectedResolver(
       async (_, { file, caption }, { loggedInUser }) => {
+        let hashtagObj = [];
         if (caption) {
           const hashtags = caption.match(/#[\w]+/g);
-          client.photo.create({
-            data: {
-              file,
-              caption,
-              hashtags: {
-                connectOrCreate: [
-                  {
-                    where: {
-                      hashtag: "#food",
-                    },
-                    create: {
-                      hashtag: "#food",
-                    },
-                  },
-                ],
+          hashtagObj = hashtags.map((hashtag) => {
+            return { where: { hashtag }, create: { hashtag } };
+          });
+          console.log(hashtagObj);
+        }
+        return client.photo.create({
+          data: {
+            file,
+            caption,
+            user: {
+              connect: {
+                id: loggedInUser.id,
               },
             },
-          });
-        }
+            ...(hashtagObj.length > 0 && {
+              hashtags: {
+                connectOrCreate: hashtagObj,
+              },
+            }),
+          },
+        });
         // save the photo with the parsed hashtags
         // add the photo to the hashtags
       }

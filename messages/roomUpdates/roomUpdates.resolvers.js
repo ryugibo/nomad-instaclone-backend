983fe79d3a1ab6332739365132676206a96bc333
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-05 15:16:15 +0900

    :memo: #7.9 | Filtering Subscriptions part One

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -1,10 +1,16 @@
+import { withFilter } from "apollo-server-express";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
 
 export default {
   Subscription: {
     roomUpdates: {
-      subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(NEW_MESSAGE),
+        ({ roomUpdates: { roomId } }, { id }) => {
+          return roomId === id;
+        }
+      ),
     },
   },
 };

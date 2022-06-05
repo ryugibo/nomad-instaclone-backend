c23d405aeb81e7543d120356246a5363019c88bd
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-05 12:41:26 +0900

    :memo: #6.18 | protectedResolver Refactor

--- users/users.utils.js
+++ users/users.utils.js
@@ -17,10 +17,15 @@ export const getUser = async (token) => {
 export function protectedResolver(ourResolver) {
   return function (root, args, context, info) {
     if (!context.loggedInUser) {
-      return {
-        ok: false,
-        error: "Please log in to perform this action.",
-      };
+      const isQuery = info.operation.operation === "query";
+      if (isQuery) {
+        return null;
+      } else {
+        return {
+          ok: false,
+          error: "Please log in to perform this action.",
+        };
+      }
     }
     return ourResolver(root, args, context, info);
   };
